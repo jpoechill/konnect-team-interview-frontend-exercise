0398683bffe625382fa242583fc7079d95a69252
jpoechill <poamrong@rith.com>
2022-06-28 01:23:12 -0700

    feat(servicenewmodal.vue): add modal for registering new services

---
+++ src/components/modals/ServiceNewModal.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal service">
+      <header class="header-container">
+        <h3>
+          Add New Service
+        </h3>
+        <button type="button" @click="handleCloseModal" class="btn">
+          <img src="/icon-close.svg" alt="Close Modal">
+        </button>
+      </header>
+      <section class="w-full">
+        <div>
+          <label for="serviceName">
+            Service Name
+          </label>
+          <input type="text" v-model="serviceName" class="input-text w-full">
+        </div>
+
+        <div>
+          <label for="serviceDescription">
+            Service Description 
+          </label>
+          <textarea v-model="serviceDescription" class="w-full" rows="4"></textarea>
+        </div>
+
+        <div>
+          <label for="versionNumber">
+            Number of Versions
+          </label>
+          <input v-model="serviceVersion" type="number" class="input-text w-full">
+        </div>
+      </section>
+      <footer class="w-full">
+        <br>
+        <button  @click="handleCloseModal" class="button-add-new w-full">
+          Add New Service
+        </button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { ref } from 'vue'
+  export default {
+    name: 'ServiceAddNew',
+    setup(_, context) {
+      const serviceName = ref('')
+      const serviceDescription = ref('')
+      const serviceVersion = ref(1)
+
+      const handleCloseModal = () => context.emit('close');
+
+      return {
+        handleCloseModal,
+        serviceName,
+        serviceDescription,
+        serviceVersion
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+.modal-backdrop {
+  height: 100vh;
+  overflow-y: hidden;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(51, 51, 51, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.w-full {
+  width: 100%;
+}
+
+.modal {
+  background: #FFFFFF;
+  border-radius: 3px;
+  width: 420px;
+}
+
+.header-container {
+  display: flex; 
+  justify-content: space-between; 
+  width: 100%;
+  
+  h3 {
+    margin: 0px;
+    color: #0A2B66;
+  }
+}
+
+section {
+  label {
+    font-size: 13px;
+    margin-top: 20px;
+    margin-bottom: 4px;
+    color: #828FA3;
+    display: block;
+  }
+}
+
+.pb-sml {
+  padding-bottom: 20px;
+}
+
+.input-text, textarea {
+  border: 1px solid #EAEAEA;
+  border-radius: 3px;
+  padding: 12px;
+  font-size: 16px;
+  margin-top: 4px;
+  font-family: 'Roboto', 'Open Sans', 'Helvetica Neue';
+  box-sizing: border-box;
+}
+
+.button-add-new {
+  font-size: 16px;
+  color: #FFF;
+  padding: 12px;
+  margin-top: 10px;
+  background-color: #1456CB;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+</style>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,7 +1,8 @@
 <template>
   <ServiceCatalog @show-modal="showModal" />
   <ServicePagination />
-  <ServiceModal v-show="isModalVisible" @close="hideModal" :serviceData="serviceData" />
+  <ServiceModal v-show="isModalVisible" @close="hideModals" :serviceData="serviceData" />
+  <ServiceAddNew v-show="showNewServiceModal"  @close="hideModals" />
 </template>
 
 <script lang="ts">
@@ -10,6 +11,7 @@ import { useServiceStore } from '@/stores/services'
 import ServiceCatalog from '@/components/ServiceCatalog.vue'
 import ServicePagination from '@/components/ServicePagination.vue'
 import ServiceModal from '@/components/ServiceModal.vue';
+import ServiceAddNew from '@/components/modals/ServiceNewModal.vue';
 import useServices from '@/composables/useServices'
 
 export default defineComponent({
@@ -17,19 +19,22 @@ export default defineComponent({
   components: {
     ServiceCatalog,
     ServicePagination,
-    ServiceModal
+    ServiceModal,
+    ServiceAddNew
   },
   setup() {
     const { services, loading } = useServices()
     const serviceStore = useServiceStore()
     const isModalVisible = ref<boolean>(false);
+    const showNewServiceModal = ref<boolean>(false);
     const serviceData = ref<object>({});
 
     serviceStore.services = services
     serviceStore.loading = loading
 
-    function hideModal() {
+    function hideModals() {
       isModalVisible.value = false;
+      showNewServiceModal.value = false
       document.body.style.overflow = 'unset';
     }
 
@@ -39,9 +44,16 @@ export default defineComponent({
       document.body.style.overflow = 'hidden';
     }
 
+    function handleShowModalNew() {
+      showNewServiceModal.value = true;
+      document.body.style.overflow = 'hidden';
+    }
+
     return {
       showModal,
-      hideModal,
+      showNewServiceModal,
+      handleShowModalNew,
+      hideModals,
       isModalVisible,
       serviceData,
     }
